f313c238cb880774615c2e6fe2ed5f2711478d98
Tarun200999 <tk4977163@gmail.com>
2022-06-19 13:37:01 +0530

    Null Pointer Exception Removed

--- src/main/java/com/backend/backend/controller/MyController.java
+++ src/main/java/com/backend/backend/controller/MyController.java
@@ -1,23 +1,25 @@
 package com.backend.backend.controller;
 import com.backend.backend.services.CourseServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.backend.backend.entities.Courses;
-
 import java.util.List;
 
 @RestController
 public class MyController {
+
+    @Autowired
     private CourseServices courseServices;
 
     @GetMapping("/test")
-    public String home(){
+    public String testApp(){
         return "Spring Boot Application is Working";
     }
 
     @GetMapping("/courses")
-    public List<Courses> createHome(){
-        return this.courseServices.getCourses();
+    public List<Courses> getCourses(){
+        return courseServices.getCourses();
     }
 }
